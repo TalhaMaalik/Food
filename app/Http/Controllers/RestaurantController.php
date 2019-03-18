984a39ada3bbc8a4641075ed956201c16db81af5
TalhaMaalik <k163694@nu.edu.pk>
2019-03-18 23:49:19 +0500

    fixed

--- app/Http/Controllers/RestaurantController.php
+++ app/Http/Controllers/RestaurantController.php
@@ -7,41 +7,47 @@ use App\itemmodel;
 use App\menumodel;
 use App\ownermodel;
 use App\restaurantmodel;
+use Illuminate\Pagination\PaginationServiceProvider;
 use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Validator;
 
 class RestaurantController extends Controller
 {
 
-    function RegisterView(){
+    function RegisterView()
+    {
         return view('RestaurantViews.registration');
     }
 
-    function ModifyView() {
-        $add_ons['restobj'] = restaurantmodel::orderby('id','asc')->paginate(5);
-        return view('RestaurantViews.modify')->with('add_ons',$add_ons);
+    function ModifyView()
+    {
+        $add_ons['restobj'] = restaurantmodel::orderby('id', 'asc')->paginate(5);
+        return view('RestaurantViews.modify')->with('add_ons', $add_ons);
     }
 
-    function EditView($rest_id) {
-        $add_ons['restobj'] = restaurantmodel::where('id','=',$rest_id)->first();
-        $add_ons['ownerobj'] = ownermodel::where('id','=',$add_ons['restobj']->ownerID)->first();
-        $add_ons['menuobj'] = menumodel::where('restaurantID','=',$rest_id)->get();
-        return view('RestaurantViews.edit')->with('add_ons',$add_ons);
+    function EditView($rest_id)
+    {
+        $add_ons['restobj'] = restaurantmodel::where('id', '=', $rest_id)->first();
+        $add_ons['ownerobj'] = ownermodel::where('id', '=', $add_ons['restobj']->ownerID)->first();
+        $add_ons['menuobj'] = menumodel::where('restaurantID', '=', $rest_id)->get();
+        return view('RestaurantViews.edit')->with('add_ons', $add_ons);
     }
 
-    function search() {
+    function search()
+    {
         try {
             $rest_id = Input::get('search');
-            $add_ons['restobj']  = restaurantmodel::where('id','=',$rest_id)->orderby('id','asc')->paginate(5);
-            return view('RestaurantViews.modify')->with('add_ons',$add_ons);
+            $add_ons['restobj'] = restaurantmodel::where('id', '=', $rest_id)->orderby('id', 'asc')->paginate(5);
+            return view('RestaurantViews.modify')->with('add_ons', $add_ons);
 
         } catch (\Exception $e) {
             $add_ons['error'] = 'No Records Found';
-            return redirect('/modify')->with('add_ons',$add_ons);
+            return redirect('/modify')->with('add_ons', $add_ons);
         }
     }
 
-    function Register(Request $request) {
+    function Register(Request $request)
+    {
 
         $owner_name = Input::get('owner_name');
         $email = Input::get('email');
@@ -54,8 +60,8 @@ class RestaurantController extends Controller
         $d_charges = Input::get('deliverycharges');
         $d_time = Input::get('deliverytime');
 
-        $errors=[];
-        $validator = Validator::make($request->all(),[
+        $errors = [];
+        $validator = Validator::make($request->all(), [
             'owner_name' => 'required',
             'email' => 'required|email',
             'owner_phone' => 'required',
@@ -70,7 +76,7 @@ class RestaurantController extends Controller
         ]);
 
         if ($validator->fails()) {
-            $errors=[];
+            $errors = [];
             $error = ($validator->messages());
 
             foreach ($error->all() as $message) {
@@ -78,14 +84,14 @@ class RestaurantController extends Controller
             }
         }
 
-        foreach(Input::get('item') as $item){
+        foreach (Input::get('item') as $item) {
 
-            if(!isset($item['name'])){
+            if (!isset($item['name'])) {
 
                 $errors[] = ("Item names are empty");
 
             }
-            if(!isset($item['price'])){
+            if (!isset($item['price'])) {
 
                 $errors[] = ("Item prices are empty");
 
@@ -105,7 +111,7 @@ class RestaurantController extends Controller
             $owner_obj->phone = $owner_phone;
             $owner_obj->Save();
 
-            $rest_obj->ownerID=$owner_obj->id;
+            $rest_obj->ownerID = $owner_obj->id;
             $rest_obj->name = $restaurant_name;
             $rest_obj->address = $address;
             $rest_obj->phone = $rest_phone;
@@ -116,29 +122,33 @@ class RestaurantController extends Controller
 
             $rest_obj->Save();
 
-            foreach(Input::get('item') as $index=> $i) {
+
+            foreach (Input::get('item') as $index => $i) {
                 $menu_obj = new menumodel();
-                $menu_obj->restaurantID=$rest_obj->id;
+                $menu_obj->restaurantID = $rest_obj->id;
                 $menu_obj->name = $i['name'];
                 $menu_obj->price = $i['price'];
 
                 $menu_obj->save();
 
-                if(isset(Input::file('item')[$index]['image'])) {
+                if (isset(Input::file('item')[$index]['image'])) {
                     $file = Input::file('item')[$index]['image'];
                     $filename = $menu_obj->id . '.' . $file->getClientOriginalExtension();
                     $file->move(public_path() . '/images/data/', $filename);
 
                 }
             }
+
+
             $add_ons['success'] = "Successfully Added";
         }
-        return view('RestaurantViews.registration')->with('add_ons',$add_ons);
+        return view('RestaurantViews.registration')->with('add_ons', $add_ons);
     }
 
 
+    function Edit(Request $request, $rest_id)
+    {
 
-    function Edit(Request $request, $rest_id) {
 
         $owner_name = Input::get('owner_name');
         $email = Input::get('email');
@@ -151,8 +161,8 @@ class RestaurantController extends Controller
         $d_charges = Input::get('deliverycharges');
         $d_time = Input::get('deliverytime');
 
-        $errors=[];
-        $validator = Validator::make($request->all(),[
+        $errors = [];
+        $validator = Validator::make($request->all(), [
             'owner_name' => 'required',
             'email' => 'required|email',
             'owner_phone' => 'required',
@@ -167,7 +177,7 @@ class RestaurantController extends Controller
         ]);
 
         if ($validator->fails()) {
-            $errors=[];
+            $errors = [];
             $error = ($validator->messages());
 
             foreach ($error->all() as $message) {
@@ -176,8 +186,8 @@ class RestaurantController extends Controller
         }
 
 
-        $rest_obj = restaurantmodel::where('id',$rest_id)->first();
-        $owner_obj = ownermodel::where('id','=',$rest_obj->ownerID)->first();
+        $rest_obj = restaurantmodel::where('id', $rest_id)->first();
+        $owner_obj = ownermodel::where('id', '=', $rest_obj->ownerID)->first();
         $add_ons['restobj'] = $rest_obj;
         $add_ons['ownerobj'] = $owner_obj;
 
@@ -192,7 +202,7 @@ class RestaurantController extends Controller
             $owner_obj->Save();
             $add_ons['ownerobj'] = $owner_obj;
 
-            $rest_obj->ownerID=$owner_obj->id;
+            $rest_obj->ownerID = $owner_obj->id;
             $rest_obj->name = $restaurant_name;
             $rest_obj->address = $address;
             $rest_obj->phone = $rest_phone;
@@ -203,53 +213,60 @@ class RestaurantController extends Controller
             $rest_obj->Save();
             $add_ons['restobj'] = $rest_obj;
 
-            foreach(Input::get('item') as $index=> $i) {
-                $menu_obj = new menumodel();
-                $menu_obj->restaurantID=$rest_obj->id;
-                $menu_obj->name = $i['name'];
-                $menu_obj->price = $i['price'];
-                $menu_obj->save();
-                $add_ons['menuobj'] = menumodel::where('restaurantID','=',$rest_id)->get();
 
-                if(isset(Input::file('item')[$index]['image'])) {
-                    $file = Input::file('item')[$index]['image'];
-                    $filename = $menu_obj->id . '.' . $file->getClientOriginalExtension();
-                    $file->move(public_path() . '/images/data/', $filename);
+            foreach (Input::get('item') as $index => $i) {
+                if ($i['name'] && $i['price']) {
 
+                    $menu_obj = new menumodel();
+                    $menu_obj->restaurantID = $rest_obj->id;
+                    $menu_obj->name = $i['name'];
+                    $menu_obj->price = $i['price'];
+                    $menu_obj->save();
+
+
+                    if (isset(Input::file('item')[$index]['image'])) {
+                        $file = Input::file('item')[$index]['image'];
+                        $filename = $menu_obj->id . '.' . $file->getClientOriginalExtension();
+                        $file->move(public_path() . '/images/data/', $filename);
+
+                    }
                 }
             }
+
+            $add_ons['menuobj'] = menumodel::where('restaurantID', '=', $rest_id)->get();
             $add_ons['message'] = "Successfully Updated";
         }
-        return view('RestaurantViews.edit')->with('add_ons',$add_ons);
+
+        return view('RestaurantViews.edit')->with('add_ons', $add_ons);
     }
 
 
-    function delete($rest_id) {
+    function delete($rest_id)
+    {
 
         try {
-            $add_ons['restobj'] = restaurantmodel::where('id','=',$rest_id)->first();
-            $add_ons['ownerobj'] = ownermodel::where('id','=',$add_ons['restobj']->ownerID)->delete();
-            $add_ons['menuobj'] = menumodel::where('restaurantID','=',$rest_id)->delete();
-            restaurantmodel::where('id','=',$rest_id)->delete();
+            $add_ons['restobj'] = restaurantmodel::where('id', '=', $rest_id)->first();
+            $add_ons['ownerobj'] = ownermodel::where('id', '=', $add_ons['restobj']->ownerID)->delete();
+            $add_ons['menuobj'] = menumodel::where('restaurantID', '=', $rest_id)->delete();
+            restaurantmodel::where('id', '=', $rest_id)->delete();
             $add_ons = ['message' => 'restaurant deleted successfully'];
-            return redirect('/modify')->with('add_ons',$add_ons);
-        }
-        catch (\Exception $e) {
+            return redirect('/modify')->with('add_ons', $add_ons);
+        } catch (\Exception $e) {
             $add_ons = ['error' => 'unable to delete the restaurant'];
-            return redirect('/modify')->with('add_ons',$add_ons);
+            return redirect('/modify')->with('add_ons', $add_ons);
         }
     }
 
-    function delete_menu($menu_id) {
+    function delete_menu($menu_id)
+    {
 
         try {
-            menumodel::where('id','=',$menu_id)->delete();
+            menumodel::where('id', '=', $menu_id)->delete();
             $add_ons = ['message' => 'menu deleted successfully'];
-            return redirect('/modify')->with('add_ons',$add_ons);
-        }
-        catch (\Exception $e) {
+            return redirect('/modify')->with('add_ons', $add_ons);
+        } catch (\Exception $e) {
             $add_ons = ['error' => 'unable to delete the restaurant'];
-            return redirect('/modify')->with('add_ons',$add_ons);
+            return redirect('/modify')->with('add_ons', $add_ons);
         }
     }
 }
